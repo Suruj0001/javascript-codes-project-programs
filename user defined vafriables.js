8f0a9e15489567eba425426bff2608f1efc77b19
SURUJ_ KALITA <108216290+Suruj0001@users.noreply.github.com>
2022-11-02 19:21:58 +0530

    Add files via upload

---
+++ user defined vafriables.js
@@ -0,0 +1,32 @@
+//Objects
+let animal = {
+    name: "cow",
+    legs: 4
+}
+
+console.log(animal); // display property
+console.log(animal.name); // dot notation
+
+//Arrays
+let selectBooks = ["The Rich Dad Poor Dad" , "Think and Grow Rich"]
+console.log(selectBooks)
+console.log(selectBooks[0]) // printing index values
+console.log(selectBooks.length)
+
+
+//Function
+
+function namasteWorld(name){
+    console.log(" NAMASTE " + name);
+}
+
+namasteWorld("SURUJ") // Its importnat call the function either no use of making it
+
+function Add(a,b){
+    return a+b;
+}
+
+console.log(Add(5,5)) // () imp
+
+
+
